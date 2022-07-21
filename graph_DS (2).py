0214a3fe54bc6253ea2a7726c79043b0d692e3e0
mallikharjuna160003 <sunkaramallikharjuna90@gmail.com>
2022-07-21 23:33:22 +0530

    multithreading concept

---
+++ graph_DS (2).py
@@ -0,0 +1,42 @@
+from collections import deque
+vertices = {"A", "B", "C", "D", "E", "F"}
+edges = {("A", "D"), ("A", "B"), ("A", "E"), ("A", "F"), ("B", "F"), ("B", "C")}
+graph = dict()
+for vertex in vertices:
+    graph[vertex] = []
+for edge in edges:
+    v1,v2 = edge
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+bfslist = []
+visitedmap =dict()
+
+def Bfs(source):
+    
+    q = deque()
+    visited = [False]*max(len(graph),1)
+    vertices1 = list(vertices)
+    for i in range(len(vertices1)):
+        visitedmap[vertices1[i]] = False
+    print(visitedmap)
+    q.append(source)
+    
+    while q:
+        s = q.popleft()
+        print(s)
+        bfslist.append(s)
+        visitedmap[s]=True
+        for nodes in graph[s]:
+            if visitedmap[nodes]==False:
+                q.append(nodes)
+                print("Q",q)
+                visitedmap[nodes]=True
+    print(bfslist)
+
+Bfs("A")        
+
+    
+    
+
+print(graph)
